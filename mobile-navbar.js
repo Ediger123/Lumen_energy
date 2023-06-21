df2e416ac2ce0c9a0e0e5432635c67a57df20632
Ediger123 <gomesluizeduardo557@gmail.com>
2023-06-21 08:06:40 -0300

    3 pontos atualização java

--- mobile-navbar.js
+++ mobile-navbar.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 window.addEventListener("scroll", function() {
     var header = document.querySelector("header");
     header.classList.toggle("scrolled", window.scrollY > 0);
@@ -47,54 +46,4 @@ class MobileNavbar {
     ".nav-list",
     ".nav-list li",
   );
-=======
-window.addEventListener("scroll", function() {
-    var header = document.querySelector("header");
-    header.classList.toggle("scrolled", window.scrollY > 0);
-});
-
-class MobileNavbar {
-    constructor(mobileMenu, navList, navLinks) {
-      this.mobileMenu = document.querySelector(mobileMenu);
-      this.navList = document.querySelector(navList);
-      this.navLinks = document.querySelectorAll(navLinks);
-      this.activeClass = "active";
-  
-      this.handleClick = this.handleClick.bind(this);
-    }
-  
-    animateLinks() {
-      this.navLinks.forEach((link, index) => {
-        link.style.animation
-          ? (link.style.animation = "")
-          : (link.style.animation = `navLinkFade 0.5s ease forwards ${
-              index / 7 + 0.3
-            }s`);
-      });
-    }
-  
-    handleClick() {
-      this.navList.classList.toggle(this.activeClass);
-      this.mobileMenu.classList.toggle(this.activeClass);
-      this.animateLinks();
-    }
-  
-    addClickEvent() {
-      this.mobileMenu.addEventListener("click", this.handleClick);
-    }
-  
-    init() {
-      if (this.mobileMenu) {
-        this.addClickEvent();
-      }
-      return this;
-    }
-  }
-  
-  const mobileNavbar = new MobileNavbar(
-    ".mobile-menu",
-    ".nav-list",
-    ".nav-list li",
-  );
->>>>>>> 61dea0d32cd522dc9d346854e31dc18e82bfa1b2
   mobileNavbar.init();
